d62219c14d854e7daa7e8ef23cb6695e0498aa73
Tuba Gökarslan <tubagokarslan@gmail.com>
2025-08-13 11:10:00 +0300

    Add project files.

---
+++ CSharpEgitim301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitim301.EntityLayer.Concrete
+{
+    //Field - Variable -Property
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName {  get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
